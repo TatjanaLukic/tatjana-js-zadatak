614bc5c0f8d4794d3ba2b1fa4e0ee7df82db19ef
TatjanaLukic <id4tanja@gmail.com>
2020-09-18 10:17:37 +0200

    initial folder structure

---
+++ src/pages/CategoriesPage/CategoriesPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CategoriesPage = props => {
+    return (
+        <div>
+            CategoriesPage
+        </div>
+    )
+}
+
+CategoriesPage.propTypes = {
+
+}
+
+export default CategoriesPage
---
+++ src/pages/TopNewsPage/TopNewsPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TopNewsPage = props => {
+    return (
+        <div>
+            TopNewsPage
+        </div>
+    )
+}
+
+TopNewsPage.propTypes = {
+
+}
+
+export default TopNewsPage
