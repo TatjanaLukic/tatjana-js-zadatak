04563d08e4549f00ef94a49d1880dc068a8814ae
TatjanaLukic <id4tanja@gmail.com>
2020-09-19 19:46:49 +0200

    added more stories and image snapshots for Header

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -4,10 +4,16 @@ import { MemoryRouter } from "react-router-dom";
 
 export default {
   title: "App/Header",
-  component: Header,
-  decorators: [(getStory) => <MemoryRouter>{getStory()}</MemoryRouter>]
+  component: Header
 };
 
 const Template = () => <Header />;
 
 export const Default = Template.bind({});
+Default.decorators=  [(getStory) => <MemoryRouter initialEntries={["/TopNewsPage"]}>{getStory()}</MemoryRouter>]
+
+export const CategoriesActive = Template.bind({});
+CategoriesActive.decorators=  [(getStory) => <MemoryRouter initialEntries={["/CategoriesPage"]}>{getStory()}</MemoryRouter>]
+
+export const SearchActive = Template.bind({});
+SearchActive.decorators=  [(getStory) => <MemoryRouter initialEntries={["/SearchPage"]}>{getStory()}</MemoryRouter>]
