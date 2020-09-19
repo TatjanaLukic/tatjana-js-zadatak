77c312cfef173b6f5ce4270a06a94f75daf0954b
TatjanaLukic <id4tanja@gmail.com>
2020-09-19 19:53:18 +0200

    added more stories and image snapshots for Navbar

--- src/components/Navbar/Navbar.stories.js
+++ src/components/Navbar/Navbar.stories.js
@@ -4,10 +4,16 @@ import Navbar from "./Navbar";
 
 export default {
   title: "App/Navbar",
-  component: Navbar,
-  decorators: [(getStory) => <MemoryRouter>{getStory()}</MemoryRouter>],
+  component: Navbar
 };
 
 const Template = () => <Navbar />;
 
 export const Default = Template.bind({});
+Default.decorators=  [(getStory) => <MemoryRouter initialEntries={["/TopNewsPage"]}>{getStory()}</MemoryRouter>]
+
+export const CategoriesActive = Template.bind({});
+CategoriesActive.decorators=  [(getStory) => <MemoryRouter initialEntries={["/CategoriesPage"]}>{getStory()}</MemoryRouter>]
+
+export const SearchActive = Template.bind({});
+SearchActive.decorators=  [(getStory) => <MemoryRouter initialEntries={["/SearchPage"]}>{getStory()}</MemoryRouter>]
