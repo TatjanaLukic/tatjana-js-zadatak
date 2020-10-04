561c73c5db47d9bfffdc0528cd2f971d14c99242
TatjanaLukic <id4tanja@gmail.com>
2020-10-04 15:04:26 +0200

    improve TopNewsPage tests and snapshot

--- src/pages/TopNewsPage/TopNewsPage.test.js
+++ src/pages/TopNewsPage/TopNewsPage.test.js
@@ -1,48 +1,127 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import mockAxios from "axios";
 import { MemoryRouter } from "react-router-dom";
-import manyNewsUS from "../../__mocks__/manyNewsUS.json";
+import manyNews from "../../__mocks__/manyNews.json";
+import manyBusinessNews from "../../__mocks__/manyNewsBusinessCategory.json";
+import CountryContext from "../../context/countryContext";
 import TopNewsPage from "./TopNewsPage";
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useLocation: () => ({
-    pathname: "/TopNewsPage",
-    // state:{}
-  }),
-}));
-
-const mockUseContext = jest.fn().mockImplementation(() => ({
-  country: "us",
-  selectCountry: () => {},
-}));
-
-React.useContext = mockUseContext;
-
 describe("<TopNewsPage />", () => {
-  it("should have loading", () => {
-    render(<TopNewsPage />);
-    expect(screen.getByTestId("loading")).toBeInTheDocument();
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
-  it("should have title", async () => {
-    mockAxios.get.mockResolvedValueOnce({ data: manyNewsUS });
-    render(<TopNewsPage />, { wrapper: MemoryRouter });
+  it("by default fetches news for us, displays title and news tiles", async () => {
+    mockAxios.get.mockResolvedValueOnce({ data: manyNews });
+
+    render(
+      <CountryContext.Provider
+        value={{ country: "us", selectCountry: () => {} }}
+      >
+        <TopNewsPage />
+      </CountryContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+
+    expect(screen.getByTestId("loading")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("loading"));
+
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+
+    expect(screen.getByText("Top news from United States")).toBeInTheDocument();
+
+    const newsTile = screen.getAllByTestId("news-tile");
+    expect(newsTile).toHaveLength(20);
+  });
+
+  it("fetches news for Great Britain, displays title and news tiles", async () => {
+    mockAxios.get.mockResolvedValueOnce({ data: manyBusinessNews });
+    render(
+      <CountryContext.Provider
+        value={{ country: "gb", selectCountry: () => {} }}
+      >
+        <TopNewsPage />
+      </CountryContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+
+    expect(screen.getByTestId("loading")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("loading"));
+
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+
+    expect(screen.getByText("Top news from Great Britain")).toBeInTheDocument();
+
+    const newsTile = screen.getAllByTestId("news-tile");
+    expect(newsTile).toHaveLength(20);
+  });
+
+  it("fetches news for some category, displays title and news tiles", async () => {
+    const state = {
+      categoryName: "business",
+    };
+    mockAxios.get.mockResolvedValueOnce({ data: manyBusinessNews });
+
+    render(
+      <CountryContext.Provider
+        value={{ country: "gb", selectCountry: () => {} }}
+      >
+        <MemoryRouter initialEntries={[{ state: { ...state } }]}>
+          <TopNewsPage />
+        </MemoryRouter>
+      </CountryContext.Provider>
+    );
+
+    expect(screen.getByTestId("loading")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("loading"));
+
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
 
     expect(
-      await screen.findByText("Top news from United States")
+      screen.getByText("Top business news from Great Britain")
     ).toBeInTheDocument();
+
+    const newsTile = screen.getAllByTestId("news-tile");
+    expect(newsTile).toHaveLength(20);
   });
 
-  it("should fetch news and display newsTiles", async () => {
-    mockAxios.get.mockResolvedValueOnce({ data: manyNewsUS });
+  it("to match loading spanshot ", async () => {
+    mockAxios.get.mockResolvedValueOnce({ data: manyNews });
 
-    render(<TopNewsPage />, { wrapper: MemoryRouter });
+    const { container } = render(
+      <CountryContext.Provider
+        value={{ country: "us", selectCountry: () => {} }}
+      >
+        <TopNewsPage />
+      </CountryContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
 
-    const newsTile = await screen.findAllByTestId("news-tile");
-    expect(newsTile).toHaveLength(20);
+    expect(container).toMatchSnapshot();
+  });
 
-    expect(mockAxios.get).toHaveBeenCalled();
+  it("to match spanshot after fetchNews ", async () => {
+    mockAxios.get.mockResolvedValueOnce({ data: manyNews });
+
+    const { container } = render(
+      <CountryContext.Provider
+        value={{ country: "us", selectCountry: () => {} }}
+      >
+        <TopNewsPage />
+      </CountryContext.Provider>,
+      { wrapper: MemoryRouter }
+    );
+
+    await waitForElementToBeRemoved(() => screen.getByTestId("loading"));
+
+    expect(container).toMatchSnapshot();
   });
 });
